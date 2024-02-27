8463d28206e415bf834d6f4f1215190d332c250b
Robin Parker <robin.parker@theolymp.net>
2024-02-27 15:27:42 +0100

    FtpServerBuilder: code cleanup

--- VoDA.FtpServer/FtpServerBuilder.cs
+++ VoDA.FtpServer/FtpServerBuilder.cs
@@ -13,21 +13,14 @@ namespace VoDA.FtpServer
     public class FtpServerBuilder
     {
 #nullable disable
-        private FtpServerOptions _serverOptions;
-        private AuthorizationOptionsContext _serverAuthorization;
+        private readonly FtpServerOptions _serverOptions = new();
+        private AuthorizationOptionsContext _serverAuthorization = new FtpServerAuthorizationOptions();
         private CertificateOptionsContext _serverCertificate;
         private FileSystemOptionsContext _serverFileSystemOptions;
-        private AccessControlOptionsContext _serverAccessControl;
-        private FtpServerLogOptions _serverLogOptions;
+        private AccessControlOptionsContext _serverAccessControl = new FtpServerAccessControlOptions();
+        private readonly FtpServerLogOptions _serverLogOptions = new();
         private FtpServer _server;
 #nullable enable
-        public FtpServerBuilder()
-        {
-            _serverOptions = new FtpServerOptions();
-            _serverLogOptions = new FtpServerLogOptions();
-            _serverAuthorization = new FtpServerAuthorizationOptions();
-            _serverAccessControl = new FtpServerAccessControlOptions();
-        }
 
         /// <summary>
         /// Configures the operation of the log system.
@@ -35,7 +28,7 @@ namespace VoDA.FtpServer
 		/// <returns>A builder object.</returns>
         public FtpServerBuilder Log(Action<IFtpServerLogOptions> config)
         {
-            config?.Invoke(_serverLogOptions);
+            config.Invoke(_serverLogOptions);
             return this;
         }
 
@@ -45,7 +38,7 @@ namespace VoDA.FtpServer
         /// <returns>A builder object.</returns>
         public FtpServerBuilder ListenerSettings(Action<IFtpServerOptions> config)
         {
-            runAndValid(config, _serverOptions);
+            RunAndValid(config, _serverOptions);
             return this;
         }
 
@@ -53,6 +46,7 @@ namespace VoDA.FtpServer
         /// Configures the operation of the authorization.
         /// </summary>
         /// <returns>A builder object.</returns>
+        // ReSharper disable once UnusedMember.Global
         public FtpServerBuilder Authorization(Action<IFtpServerAuthorizationOptions> config)
         {
             var data = new FtpServerAuthorizationOptions();
@@ -80,7 +74,7 @@ namespace VoDA.FtpServer
         public FtpServerBuilder Certificate(Action<IFtpServerCertificateOptions> config)
         {
             var data = new FtpServerCertificateOptions();
-            config?.Invoke(data);
+            config.Invoke(data);
             if (!string.IsNullOrWhiteSpace(data.CertificatePath) &&
                 !string.IsNullOrWhiteSpace(data.CertificateKey))
             {
@@ -99,8 +93,8 @@ namespace VoDA.FtpServer
                 !File.Exists(data.CertificateKey))
             {
                 // https://stackoverflow.com/a/52535184
-                var ecdsa = ECDsa.Create();
-                var req = new CertificateRequest("cn=VoDA.FTP", ecdsa, HashAlgorithmName.SHA256);
+                var ecdSa = ECDsa.Create();
+                var req = new CertificateRequest("cn=VoDA.FTP", ecdSa, HashAlgorithmName.SHA256);
                 var cert = req.CreateSelfSigned(DateTimeOffset.Now, DateTimeOffset.Now.AddYears(5));
                 File.WriteAllBytes(data.CertificateKey, cert.Export(X509ContentType.Pfx, "637925437145433542"));
 
@@ -118,6 +112,7 @@ namespace VoDA.FtpServer
         /// </summary>
         /// <typeparam name="T">Must inherit from <see cref="CertificateOptionsContext"/> and have a <c>new()</c> implementation</typeparam>
         /// <returns>A builder object.</returns>
+        // ReSharper disable once UnusedMember.Global
         public FtpServerBuilder Certificate<T>() where T : CertificateOptionsContext, new()
         {
             _serverCertificate = new T();
@@ -152,6 +147,7 @@ namespace VoDA.FtpServer
         /// Configures access to the server.
         /// </summary>
         /// <returns>A builder object.</returns>
+        // ReSharper disable once UnusedMember.Global
         public FtpServerBuilder AccessControl(Action<IFtpServerAccessControlOptions> config)
         {
             var data = new FtpServerAccessControlOptions();
@@ -165,6 +161,7 @@ namespace VoDA.FtpServer
         /// </summary>
         /// <typeparam name="T">Must inherit from <see cref="AccessControlOptionsContext"/> and have a <c>new()</c> implementation</typeparam>
         /// <returns>A builder object.</returns>
+        // ReSharper disable once UnusedMember.Global
         public FtpServerBuilder AccessControl<T>() where T : AccessControlOptionsContext, new()
         {
             _serverAccessControl = new T();
@@ -190,16 +187,17 @@ namespace VoDA.FtpServer
         public IFtpServerControl Build()
         {
             if (_serverFileSystemOptions == null)
-                throw new ArgumentNullException("The algorithm for processing requests to work with the file system is not specified!");
+                throw new ArgumentNullException(nameof(_serverFileSystemOptions),"The algorithm for processing requests to work with the file system is not specified!");
             if (_serverCertificate == null)
-                throw new ArgumentNullException("Security certificate file names are not specified!");
+                throw new ArgumentNullException(nameof(_serverCertificate), "Security certificate file names are not specified!");
             _server = new FtpServer(new FtpServerParameters(_serverOptions, _serverAuthorization, _serverFileSystemOptions, _serverCertificate, _serverLogOptions, _serverAccessControl));
             return _server;
         }
 
-        private void runAndValid<T, I>(Action<T> action, I type) where I : IValidConfig, T
+        private static void RunAndValid<TAction, TType>(Action<TAction> action, TType type) 
+            where TType : IValidConfig, TAction
         {
-            action?.Invoke(type);
+            action.Invoke(type);
             type.Valid();
         }
     }
